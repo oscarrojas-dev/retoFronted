944710d2a37f0ec43a6b843d647c08106697f90a
oscar rojas <oscar.rojas@oeroweb.com>
2021-02-10 08:19:11 -0500

    estilos del modal

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var verPaises = async () =>{
   contenedor.innerHTML = '';
   
   for( let i in pais){        
-      contenedor.innerHTML +=  ("<div class='item_Content'><div class='title'><a href='#' data-name='" + pais[i].name +"' onclick=abrirModal()><div> <i class='fas fa-globe-americas'></i> </div>"  + pais[i].name + " </a></div><div class='body-content'><i class='fas fa-map-marked'></i> <strong>Capital </strong> : "  + pais[i].capital + " </div><div class=''> <i class='fas fa-users'></i> <strong> Población Actual </strong>: "  + pais[i].population + " </div></div>");       
+      contenedor.innerHTML +=  ("<div class='item_Content'><div class='title'><a href='#' data-name='" + pais[i].name +"' onclick=abrirModal()><div> <i class='fas fa-globe-americas'></i> </div>"  + pais[i].name + " </a></div><div class='text'><i class='fas fa-map-marked'></i> <strong>Capital </strong> : "  + pais[i].capital + " </div><div class='text'> <i class='fas fa-users'></i> <strong> Población Actual </strong>: "  + pais[i].population + " </div></div>");       
   }  
 }
 
