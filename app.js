855d4983f09451aee74871c71d54201e7e2416fc
oscar rojas <oscar.rojas@oeroweb.com>
2021-02-20 19:39:16 -0500

    agregando  comnetarios al app.j

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
 // console.log("conectado");
 
-
-
 // Realizamos la peticion fecth
 var verPaises = async () =>{
   var api = await fetch("https://restcountries.eu/rest/v2/lang/es");
   const pais = await api.json();
-  console.log(pais);
 
   var contenedor = document.getElementById("MainContent");
   contenedor.innerHTML = '';
-  
-  for( let i in pais){        
-      contenedor.innerHTML +=  ("<div class='item_Content'><div class='title'><a href='#' data-name='" + pais[i].name +"' onclick=abrirModal()><div> <i class='fas fa-globe-americas'></i> </div>"  + pais[i].name + " </a></div><div class='text'><i class='fas fa-map-marked'></i> <strong>Capital </strong> : "  + pais[i].capital + " </div><div class='text'> <i class='fas fa-users'></i> <strong> Población Actual </strong>: "  + pais[i].population + " </div></div>");       
-  }  
+  modal = document.getElementById("modal");
+  modal.innerHTML = "";
+   
+  // insertamos los paise en el id MainContent
+  pais.forEach((el, index) => {
+    contenedor.innerHTML +=  (`
+    <div class="item_Content" >
+      <div class='title'>
+        <a href='#' data-name="${el.name}" id="${index}" onclick=abrirModal()> 
+        <div><i class='fas fa-globe-americas'></i></div> ${el.name} </a>
+      </div>
+      <div class='text'>
+        <i class='fas fa-map-marked'></i> <strong>Capital </strong> : "${el.capital}"
+      </div>
+      <div class='text'> 
+        <i class='fas fa-users'></i> <strong> Población Actual </strong>: ${el.population}
+      </div>
+    </div>`);
+    
+  });
+
+  // Insertamos datos a ventana modal
+  pais.forEach((el, index) =>{
+    modal.innerHTML += `
+      <div class="body-modal">				
+			  <h2 id="pais ${index}">${el.name} </h2><hr>
+        <div class="inner-modal">					
+          <span class="" id="capital">Capital : ${el.capital}</span>	
+          <span class="" id="poblacion">Pobalcion : ${el.population}</span>			
+        </div>			
+			  <a href="#" class="btncerrar" onclick="cerraModal()"> Cerrar</a>	
+	    </div>`;
+  })
 }
 
+// llamar a la funcion
 verPaises();
+
+// Abrir modal
+function abrirModal(){   
+  modal.style.display = "flex";
+}
+
+// cerrar modal
+function cerraModal(){   
+  modal.style.display = "none";
+}
